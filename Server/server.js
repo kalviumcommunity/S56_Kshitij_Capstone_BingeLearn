9928bf0cc51fa8f513ec51bb989c3fd11cd66504
Kshitij Kotecha <kshitijkotecha@Kshitijs-MacBook-Air.local>
2024-04-04 10:22:19 +0530

    Removed the redundant require('dotenv').config(); statement

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 // Import Statements
+require('dotenv').config();
 const express = require('express');
-require('dotenv').config()
+const mongoose = require('mongoose');
 const Router = require('./routes')
 const { connected, isConnected } = require('./config/db.js');
 const cors = require('cors');
