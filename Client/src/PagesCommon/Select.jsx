4f9275c741412dd4b64590593ef893c20969333c
Kshitij Kotecha <kotechakshitij@gmail.com>
2024-06-25 11:42:15 +0530

    Bruno Folder created

--- Client/src/PagesCommon/Select.jsx
+++ Client/src/PagesCommon/Select.jsx
@@ -1,41 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import Navbar from '../Components/Navbar';
-import school from '../Images/school.png';
-import './Select.css';
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import Navbar from "../Components/Navbar";
+import laptop from "../Images/laptop.png";
+import "./Select.css";
 
 const Select = () => {
   const handleUserTypeSelection = (userType) => {
     // Store the user type in session storage
-    sessionStorage.setItem('userType', userType);
+    sessionStorage.setItem("userType", userType);
   };
 
   return (
     <>
       <Navbar />
-      <div className='quoteS'>
-        <h1 className="glow-text">"Binge Learn Your Way to Success"</h1>
-      </div>
-      <div className='SchoolS'>
-        <img id="schoolS" src={school} alt="school" />
-      </div>
-      <div className='buttonS'>
-        {/* Link for Teacher */}
-        <Link
-          className="button1S glow-on-hover"
-          to="/login"
-          onClick={() => handleUserTypeSelection('teacher')}
-        >
-          I am a Teacher
-        </Link>
-        {/* Link for Student */}
-        <Link
-          className="button2S glow-on-hover"
-          to="/login"
-          onClick={() => handleUserTypeSelection('student')}
-        >
-          I am a Student
-        </Link>
+      <div className="bodySS">
+        <div className="quoteS">
+          <h1 className="glow-text">"Binge Learn Your Way to Success"</h1>
+        </div>
+        <div className="detailSS">
+          <div className="buttonS">
+            {/* Link for Teacher */}
+            <Link
+              className="button1S glow-on-hover"
+              to="/login"
+              onClick={() => handleUserTypeSelection("teacher")}
+            >
+              I am a Teacher
+            </Link>
+            {/* Link for Student */}
+            <Link
+              className="button2S glow-on-hover"
+              to="/login"
+              onClick={() => handleUserTypeSelection("student")}
+            >
+              I am a Student
+            </Link>
+          </div>
+          <div className="SchoolS">
+            <img src={laptop} alt="school" />
+          </div>
+        </div>
       </div>
     </>
   );
